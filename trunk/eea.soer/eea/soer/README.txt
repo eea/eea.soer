a8b5ec1bdffbf296db8cc4b5d01c7773e6b6ef1a
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-07-30 08:08:47 +0000

    Started working on AT schema / web form

--- trunk/eea.soer/eea/soer/README.txt
+++ trunk/eea.soer/eea/soer/README.txt
@@ -22,3 +22,35 @@ interface:
   >>> print report
   <SOERReport at testreport>
 
+
+User Interface
+--------------
+
+SOERReports are added through a web form:
+
+  >>> from Products.Five.testbrowser import Browser
+  >>> from Products.PloneTestCase.setup import portal_owner, default_password
+  >>> browser = Browser()
+  >>> browser.handleErrors = False
+
+We must log in:
+
+  >>> browser.open(self.portal.absolute_url())
+  >>> browser.getControl(name='__ac_name').value = portal_owner
+  >>> browser.getControl(name='__ac_password').value = default_password
+  >>> browser.getControl(name='submit').click()
+
+Now, when looking at the form, we can verify the existance and type of the
+expected fields:
+
+  >>> url = report.absolute_url() + '/edit'
+  >>> browser.open(url)
+  >>> form = browser.getForm('soerreport-base-edit')
+  >>> for i in 'topics', 'content_type', 'sections', 'country':
+  ...     ctl = form.getControl(name=i)
+  ...     print ctl.type
+  select
+  select
+  select
+  select
+
--- trunk/eea.soer/eea/soer/soer.py
+++ trunk/eea.soer/eea/soer/soer.py
@@ -10,9 +10,62 @@ except ImportError:
     from Products.Archetypes.public import *
 
 
+schema = Schema((
+
+    StringField(
+        name='topics',
+        widget=SelectionWidget(
+            label='Topics',
+            label_msgid='eea.soer_label_topics',
+            i18n_domain='eea.soer',
+            format='select',
+        ),
+        vocabulary=['a', 'b', 'c']
+    ),
+
+    StringField(
+        name='content_type',
+        widget=SelectionWidget(
+            label='Content Type',
+            label_msgid='eea.soer_label_content_types',
+            i18n_domain='eea.soer',
+            format='select',
+        ),
+        vocabulary=['a', 'b', 'c']
+    ),
+
+    StringField(
+        name='sections',
+        widget=SelectionWidget(
+            label='Content Type',
+            label_msgid='eea.soer_label_sections',
+            i18n_domain='eea.soer',
+            format='select',
+        ),
+        vocabulary=['a', 'b', 'c']
+    ),
+
+    StringField(
+        name='country',
+        widget=SelectionWidget(
+            label='Country',
+            label_msgid='eea.soer_label_country',
+            i18n_domain='eea.soer',
+            format='select',
+        ),
+        vocabulary=['a', 'b', 'c']
+    ),
+
+),
+)
+
+schema = getattr(ATFolder, 'schema', Schema(())).copy() + schema.copy()
+
+
 class SOERReport(ATFolder):
     """ """
     implements(ISOERReport)
+    schema = schema
     security = ClassSecurityInfo()
 
 
