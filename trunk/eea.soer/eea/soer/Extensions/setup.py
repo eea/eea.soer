2e13fb2b1af44948530637973e19c84847659542
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-08-14 07:03:25 +0000

    Set country folder title on setup so that it's obvious which country it belongs to. #2618

--- trunk/eea.soer/eea/soer/Extensions/setup.py
+++ trunk/eea.soer/eea/soer/Extensions/setup.py
@@ -1,6 +1,9 @@
-from eea.soer.vocab import european_country_codes
+from eea.soer.vocab import european_country_codes, european_countries
 
 
 def setup_folder_structure(context):
     for i in european_country_codes:
-        context.invokeFactory('Folder', id=i)
+        id = context.invokeFactory('Folder', id=i)
+        folder = context[id]
+        title = european_countries[i]
+        folder.setTitle(title)
