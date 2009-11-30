b577a3c3fd27f3549900cfae1a9c3177c4b8d24f
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-11-30 16:43:48 +0000

    Fixed outdated imports in setup script, added debug print info

--- eea/soer/Extensions/setup.py
+++ eea/soer/Extensions/setup.py
@@ -1,11 +1,12 @@
-from eea.soer.vocab import european_country_codes, european_countries
+from Products.CMFCore.utils import getToolByName
 
 
 def setup_folder_structure(context):
-    for i in european_country_codes:
-        id = context.invokeFactory('Folder', id=i)
-        folder = context[id]
-        title = european_countries[i]
-        folder.setTitle(title)
+    vocab = getToolByName(context, 'portal_vocabularies')
+    european_countries = vocab['eea.soer.vocab.european_countries']
+    for country_code in european_countries.keys():
+        print 'Creating folder with id %s and title %s' % (country_code, european_countries[country_code].title)
+        folder = context[context.invokeFactory('Folder', id=country_code)]
+        folder.setTitle(european_countries[country_code].title)
         folder.unmarkCreationFlag()
         folder.reindexObject()
