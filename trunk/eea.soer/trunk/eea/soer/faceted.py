fefd3422fc87d6ce6482a4b0027c416a42d4dea7
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-28 12:12:57 +0000

    enabled faceted on SOER Country and mockup for soercountry view

---
+++ trunk/eea.soer/trunk/eea/soer/faceted.py
@@ -0,0 +1,6 @@
+from eea.facetednavigation.subtypes.descriptors import FacetedNavigableDescriptor
+
+class FolderFacetedNavigableDescriptor(FacetedNavigableDescriptor):
+    """ Folder descriptor
+    """
+    for_portal_type = 'SOERCountry'
