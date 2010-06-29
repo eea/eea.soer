21227024be2856f9c77920ea584dec6f9fb7b221
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-06-29 17:07:25 +0000

    Tagged eea.soer 0.4.1.

---
+++ trunk/eea.soer/tags/0.4.1/eea/soer/content/SOERCountry.py
@@ -0,0 +1,184 @@
+from zope.interface import implements
+import surf
+import urllib2 
+from BeautifulSoup import BeautifulSoup
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.utils import getToolByName
+from Products.ATContentTypes.content.folder import ATFolder
+from eea.soer.content.interfaces import IReportingCountry
+from eea.soer.config import *
+from eea.soer import vocab
+from eea.soer import sense
+
+try:
+    from Products.LinguaPlone.public import *
+except ImportError:
+    # No multilingual support
+    from Products.Archetypes.public import *
+from Products.CMFPlone.log import log
+
+
+schema = Schema((
+
+    StringField(
+        name='rdfFeed',
+        languageIndependent=False,
+        widget=StringWidget(
+            size=70,
+            label='RDF feed',
+            label_msgid='label_feed_rdf_url',
+            description='The address of the feed.',
+            description_msgid='help_feed_rdf_url',
+            i18n_domain='plone',
+        ),
+    ),
+
+),
+)
+
+schema = getattr(ATFolder, 'schema', Schema(())).copy() + schema.copy()
+
+
+class SOERCountry(ATFolder):
+    """ """
+    implements(IReportingCountry)
+
+    security = ClassSecurityInfo()
+    meta_type = 'SOERCountry'
+    portal_type = 'SOERCountry'
+    allowed_content_types = ['Image', 'CommonalityReport', 'DiversityReport',
+                             'FlexibilityReport','Link']
+    _at_rename_after_creation = True
+
+    schema = schema
+    _v_feedUpdating = False
+    
+    security.declareProtected(ADD_CONTENT_PERMISSION, 'updateFromFeed')
+    def updateFromFeed(self):
+        """ update feed """
+        url = self.getRdfFeed()
+        if url:
+            self._v_feedUpdating = True
+            reports = {}
+            soer = sense.SoerRDF2Surf(url)
+            self.channel = channel = soer.channel()
+            if channel and channel.get('organisationLogoURL',None):
+                image = urllib2.urlopen(channel['organisationLogoURL'])
+                image_data = image.read()
+                if image_data:
+                    if not hasattr(self, 'logo'):
+                        logo = self[self.invokeFactory('Image', id='logo',
+                                                       image=image_data)]
+                        try: 
+                            wtool.doActionFor(logo, 'publish', comment='Automatic feed update')
+                        except:
+                            log('Failed to publish %s' % logo.absolute_url())
+                    else:
+                        logo = self['logo']
+                        logo.setImage(image_data)
+                    
+                    
+            wtool = getToolByName(self, 'portal_workflow')
+            oldContentIds = [ oId for oId, obj in self.objectItems()
+                              if obj.portal_type not in ['Image','RSSFeedRecipe']]
+            log('Deleting %s' % oldContentIds)
+            self.manage_delObjects(ids=oldContentIds)
+            parentReport = None
+            for nstory in soer.nationalStories():
+                questions = dict([[v,k] for k,v in vocab.long_diversity_questions.items()])
+                questions.update(dict([[v,k] for k,v in vocab.long_questions.items()]))
+                question = questions.get(nstory.question, '')
+                parentReport = reports.get((nstory.topic, nstory.question), None)
+                if parentReport:
+                    report = parentReport[parentReport.invokeFactory(nstory.portal_type, id='temp_report',
+                                                                     topic=nstory.topic,
+                                                                     question=question)]
+                    
+                else:
+                    report = self[self.invokeFactory(nstory.portal_type, id='temp_report',
+                                                 topic=nstory.topic,
+                                                 question=questions.get(nstory.question, ''))]
+                
+                report.setDescription(nstory.description)
+                report.setKeyMessage(nstory.keyMessage)
+                report.setGeoCoverage(nstory.geoCoverage)
+                report.setSubject(nstory.keyword)
+                newId = report._renameAfterCreation(check_auto_id=False)
+                if parentReport is None:
+                    parentReport = report = self[newId]
+                    reports[(nstory.topic, nstory.question)] = report
+                else:
+                    report = parentReport[newId]
+                    if hasattr(nstory,'sortOrder'):
+                        parentReport.moveObjectToPosition(newId, int(nstory.sortOrder))
+                    else:
+                        parentReport.moveObjectsToTop(ids=[newId])
+                        
+                assessment = nstory.assessment
+                for fig in nstory.hasFigure():
+                    log('Fetching Figure: %s' % fig['url'])
+                    # read figure
+                    try:
+                        image = urllib2.urlopen(fig['url'])
+                    except:
+                        continue
+                    image_data = image.read()
+                    if image_data:
+                        figure = getattr(report, fig['fileName'], None)
+                        if figure is not None:
+                            continue
+                        figure = report[report.invokeFactory('Image', id=fig['fileName'],
+                                                             image=image_data)]
+                        figure.setTitle(fig['caption'])
+                        figure.setDescription(fig['description'])
+                        if fig['fileName'] == 'tempfile':
+                            newId = figure._renameAfterCreation(check_auto_id=False)
+                            figure = report[newId]
+                        try:
+                            wtool.doActionFor(figure, 'publish', comment='Automatic feed update')
+                        except:
+                            log('Failed to publish %s' % figure.absolute_url())
+
+                        if fig['url'] in assessment.decode('utf8'):
+                            assessment = assessment.replace(fig['url'], 'resolveuid/%s' % figure.UID())
+                        if fig.get('dataSource', None) is not None:
+                            dataSrc = fig['dataSource']
+                            dataLink = report[report.invokeFactory('Link', id=dataSrc['fileName'],
+                                                                   title=dataSrc['fileName'],
+                                                        remoteUrl=dataSrc['dataURL'])]
+                            figure.setRelatedItems([dataLink])
+                        figure.reindexObject()
+                i = 0
+                for indicatorUrl in nstory.relatedIndicator():
+                    i += 1
+                    if not indicatorUrl.startswith('http'):
+                        # FIXME need to find out which indicator url it si for i.e CSI 018
+                        continue
+                    soup = BeautifulSoup(urllib2.urlopen(indicatorUrl))
+                    
+                    indicator = report[report.invokeFactory('RelatedIndicatorLink', id='indicator%s' % i,
+                                                            remoteUrl=indicatorUrl,
+                                                            title=soup.title.string.encode('utf8'))]
+
+                    try:
+                        wtool.doActionFor(indicator, 'publish', comment='Automatic feed update')
+                    except:
+                        log('Failed to publish %s' % indicator.absolute_url())                
+                    
+                report.setText(assessment, format='text/html')
+                report.setEffectiveDate(nstory.pubDate)
+                try:
+                    wtool.doActionFor(report, 'publish', comment='Automatic feed update')
+                except:
+                    log('Failed to publish %s' % report.absolute_url())                
+                report.original_url = nstory.subject.strip()
+                report.reindexObject()
+        self._v_feedUpdating = False
+        
+def soerCountryUpdated(obj, event):
+    if obj.getRdfFeed() and not obj._v_feedUpdating:
+        obj.updateFromFeed()
+        
+    
+
+registerType(SOERCountry, PROJECTNAME)
---
+++ trunk/eea.soer/tags/0.4.1/eea/soer/vocab.py
@@ -0,0 +1,107 @@
+from zope.app.schema.vocabulary import IVocabularyFactory
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+import surf
+
+# Maps values from eea.soer.vocab.topics to their full description
+long_topics = {
+    u'air pollution': u"Air pollution - urban and rural air quality, national and transboundary pollution, measures",
+    u'climate change': u"Climate change mitigation - GHG emissions trends and projections national measures",
+    u'biodiversity': u"Nature protection and biodiversity - protected areas, 2010 target, measures",
+    u'land': u"Land - CLC 1990-2006 - stocks, changes, drivers",
+    u'freshwater': u"Freshwaters - surface and ground, quality and quantity, WFD, measures",
+    u'waste': u"Waste - waste generation, treatment and prevention, measures",
+}
+
+# Maps values from eea.soer.vocab.questions to their full description
+long_questions = {
+    u'0': u"Why should we care?",
+    u'1': u"What are the state and impacts?",
+    u'2': u"What are the related drivers and pressures?",
+    u'3': u"What is the 2020 outlook?",
+    u'4': u"What are the policy responses?",
+}
+
+# Maps values from eea.soer.vocab.diversity_questions to their full description
+long_diversity_questions = {
+    u'10': u"What distinguishes the country?",
+    u'11': u"What are the major societal trends?",
+    u'12': u"What are the main drivers?",
+    u'13': u"What are the foreseen developments?",
+}
+
+atvocabs = {
+    'eea.soer.vocab.topics': (
+        (u'air pollution', "Air pollution"),
+        (u'climate change', "Climate change mitigation"),
+        (u'biodiversity', "Nature protection and biodiversity"),
+        (u'land', "Land"),
+        (u'freshwater', "Freshwaters"),
+        (u'waste', "Waste"),
+    ),
+
+    'eea.soer.vocab.questions': (
+        (u'0', "Why care?"),
+        (u'1', "State and impacts"),
+        (u'2', "Drivers and pressures"),
+        (u'3', "Outlook to 2020"),
+        (u'4', "Policy responses"),
+    ),
+
+    'eea.soer.vocab.diversity_questions': (
+        (u'10', 'Factors'),
+        (u'11', 'Societal developments'),
+        (u'12', 'Main drivers'),
+        (u'13', 'Main developments'),
+    ),
+
+}
+
+atvocabs['eea.soer.vocab.all_questions'] = atvocabs['eea.soer.vocab.diversity_questions'] + atvocabs['eea.soer.vocab.questions']
+
+geostore = surf.Store(reader='rdflib',  writer='rdflib', rdflib_store = 'IOMemory')
+geosession = surf.Session(geostore)
+surf.ns.register(ROD="http://rod.eionet.europa.eu/schema.rdf#")
+#geostore.load_triples(source="http://rod.eionet.europa.eu/countries")        
+atvocabs['eea.soer.vocab.geo_coverage'] = []
+Locality = geosession.get_class(surf.ns.ROD['Locality'])
+
+surf.ns.register(NUTS="http://rdfdata.eionet.europa.eu/ramon/ontology/")
+
+for loc in Locality.all().order():
+    atvocabs['eea.soer.vocab.geo_coverage'].append((loc.rod_loccode.first.strip(), loc.rdfs_label.first.strip()))
+
+
+# geostore.load_triples(source="http://rdfdata.eionet.europa.eu/ramon/send_all")
+# use local file to speed up for now
+from eea.soer.tests.base import nutsrdf
+geostore.load_triples(source=nutsrdf)
+
+
+class NUTSRegions(object):
+    """ All regions """
+
+    implements(IVocabularyFactory)
+
+    def __call__(self, context=None):
+        NUTSRegion = geosession.get_class(surf.ns.NUTS['NUTSRegion'])
+        vocabulary = []
+        for region in NUTSRegion.all().order():
+            vocabulary.append(SimpleTerm(region.subject.strip(),
+                                         token=region.nuts_code.first.strip(),
+                                         title=region.nuts_name.first.strip()))
+        return SimpleVocabulary(vocabulary)
+
+    def resources(self):
+        NUTSRegion = geosession.get_class(surf.ns.NUTS['NUTSRegion'])
+        return NUTSRegion.all().order()
+    
+    def getCode(self, subject):
+        NUTSRegion = geosession.get_class(surf.ns.NUTS['NUTSRegion'])
+        region = NUTSRegion(subject).nuts_code.first
+        if region:
+            return region.strip()
+        return ''
+    
+VocabularyFactory = NUTSRegions()
