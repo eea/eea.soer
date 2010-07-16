82956b073e8e7d71b7400b535fbe67882d26b8e1
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-07-16 08:43:26 +0000

    Tagged eea.soer 0.4.3.

---
+++ trunk/eea.soer/tags/0.4.3/eea/soer/content/DiversityReport.py
@@ -0,0 +1,53 @@
+from zope.interface import implements
+from AccessControl import ClassSecurityInfo
+from Products.ATContentTypes.configuration import zconf
+from Products.ATContentTypes.content.folder import ATFolder
+from Products.ATContentTypes.content.newsitem import ATNewsItem
+from eea.soer.content.interfaces import IDiversityReport
+from eea.soer.content.SOERReport import SOERReport
+from eea.soer.config import *
+from eea.soer import vocab
+from Products.ATVocabularyManager import NamedVocabulary
+try:
+    from Products.LinguaPlone.public import *
+except ImportError:
+    # No multilingual support
+    from Products.Archetypes.public import *
+
+schema = getattr(SOERReport, 'schema').copy() 
+schema['description'].default_method = 'default_desc'
+schema['topic'].widget.visible = { 'edit' : 0 }
+schema['topic'].default=u'country introduction'
+schema['question'].vocabulary=NamedVocabulary('eea.soer.vocab.diversity_questions')
+
+class DiversityReport(SOERReport):
+    """ """
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(SOERReport,'__implements__',()),)
+    implements(IDiversityReport)
+
+    meta_type = 'DiversityReport'
+    portal_type = 'DiversityReport'
+
+    schema = schema
+    default_view = 'diversity_report_view'
+
+    def getLongSoerQuestion(self):
+        return vocab.long_diversity_questions[self.getQuestion()]
+
+    def default_desc(self):
+        country = self.getTermTitle('eea.soer.vocab.european_countries', self.getSoerCountry())
+        desc = 'SOER Part C Diversity Report from %s' % country
+        return desc
+
+
+registerType(DiversityReport, PROJECTNAME)
+
+def gen_title(obj, evt):
+    country = obj.getTermTitle('eea.soer.vocab.european_countries', obj.getSoerCountry())
+    if obj.getTopic() == u'country introduction':
+        obj.setTitle('Country introduction (%s)' % country)
+    else:
+        question = obj.getTermTitle('eea.soer.vocab.diversity_questions', obj.getQuestion())
+        t = 'Diversity Report: %s (%s)' % (question, country)
+        obj.setTitle(t)
---
+++ trunk/eea.soer/tags/0.4.3/eea/soer/content/interfaces.py
@@ -0,0 +1,71 @@
+from zope.interface import Interface, Attribute
+from zope.schema import Choice, Bool, Set, List, TextLine, Text
+
+
+class ISOERReport(Interface):
+
+    reportingCountryCode = Attribute(u'Which country do you represent?')
+
+    geoCoverage = Choice(
+            title=u'Geographical coverage',
+            description=u'The geographical (spatial) area covered by the assessment.',
+            required=True,
+            vocabulary=u'Geographical coverage'
+            )
+
+    topic = List(
+            title=u'Topic',
+            description=u'Which topic does this report include',
+            required=True
+            )
+
+    question = Choice(
+            title=u'Question',
+            description=u'Which report section are you answering to?',
+            required=True,
+            vocabulary=u'SOER Questions'
+            )
+
+    short_topic = Attribute("Short version of the selected topic")
+    long_section = Attribute("Long version of the selected section")
+
+    description = Text(
+            title=u'Key message',
+            description=u'This is a short key message for the National Story, a kind of very short summary or teaser (one/two paragraphs)',
+            required=True
+            )
+
+    # keyMessage
+    keyMessage = Text(
+            title=u'Key message',
+            description=u'This is a short key message for the National Story, a kind of very short summary or teaser (one/two paragraphs)',
+            required=True
+            )
+
+    #assesment
+    assessment = Text(
+            title=u'Report assesment',
+            description=u'Content of this report',
+            required=True
+            )
+    
+    
+
+class ICommonalityReport(ISOERReport):
+    pass
+
+class IDiversityReport(ISOERReport):
+    pass
+
+class IFlexibilityReport(ISOERReport):
+    pass
+
+
+class IReportingCountry(Interface):
+    """ For a folder that contains SOER reports.
+        This interface is then used to create RDF from that folder for all
+        contained reports. """
+
+    def updateFromFeed():
+        """ Update reports from the rdf feed url """
+    
