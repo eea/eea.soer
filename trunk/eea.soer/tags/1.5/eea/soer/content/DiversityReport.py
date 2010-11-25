8128982f4e2c27c1ac38f4efe8552d76155f98a0
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-11-25 10:18:11 +0000

    Tagged eea.soer 1.5.

---
+++ trunk/eea.soer/tags/1.5/eea/soer/content/DiversityReport.py
@@ -0,0 +1,62 @@
+from zope.interface import implements
+from AccessControl import ClassSecurityInfo
+from Products.ATContentTypes.configuration import zconf
+from Products.ATContentTypes.content.folder import ATFolder
+from Products.ATContentTypes.content.newsitem import ATNewsItem
+from eea.soer.content.interfaces import IDiversityReport
+from eea.soer.content.SOERReport import schema as  SOERReportSchema, SOERReport
+from eea.soer.config import *
+from eea.soer import vocab
+from Products.ATVocabularyManager import NamedVocabulary
+try:
+    from Products.LinguaPlone.public import *
+except ImportError:
+    # No multilingual support
+    from Products.Archetypes.public import *
+schema = Schema((
+        StringField(
+        name='topic',
+        required = False,
+        default=u'country introduction',
+        widget=StringWidget(
+            label='Topics',
+            label_msgid='eea.soer_label_topics',
+            i18n_domain='eea.soer',
+            visible={'view' : 'invisible',
+                     'edit' : 'invisible'},
+            description='country introduction'
+         ),
+     ),
+),)
+
+schema = SOERReportSchema.copy() + schema
+schema['question'].vocabulary=NamedVocabulary('eea.soer.vocab.diversity_questions')
+
+
+class DiversityReport(SOERReport):
+    """ """
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(SOERReport,'__implements__',()),)
+    implements(IDiversityReport)
+
+    meta_type = 'DiversityReport'
+    portal_type = 'DiversityReport'
+
+    schema = schema
+    default_view = 'diversity_report_view'
+
+    def getLongSoerQuestion(self):
+        return vocab.long_diversity_questions[self.getQuestion()]
+
+    def default_desc(self):
+        country = self.getTermTitle('eea.soer.vocab.european_countries', self.getSoerCountry())
+        return 'SOER Country profile from %s' % country
+
+registerType(DiversityReport, PROJECTNAME)
+
+def reportUpdated(obj, event):
+    country = obj.getTermTitle('eea.soer.vocab.european_countries', obj.getSoerCountry())
+    question = obj.getTermTitle('eea.soer.vocab.diversity_questions', obj.getQuestion())
+    obj.setTitle('Country profile - %s (%s)' % (question, country))
+    if not obj.Description() and not obj.isTemporary():
+        obj.setDescription(obj.default_desc())
