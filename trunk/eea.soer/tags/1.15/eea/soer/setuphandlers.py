af1c1010fc9e55f4cb91819f848e346694dd5f10
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-07 11:34:41 +0000

    Tagged eea.soer 1.15.

---
+++ trunk/eea.soer/tags/1.15/eea/soer/setuphandlers.py
@@ -0,0 +1,76 @@
+from plone.i18n.locales.interfaces import ICountryAvailability
+
+from zope.component import queryUtility
+from Products.CMFCore.utils import getToolByName
+from eea.soer.vocab import atvocabs as vocabs
+from eea.vocab import countries
+from eea.soer import transform
+
+def reindexSoerReports(context):
+    portal = context.getSite()
+    catalog = portal.portal_catalog
+    result = catalog.searchResults({'object_provides': 'eea.soer.content.interfaces.ISOERReport', 'Language': 'all'})
+    for i in result:
+        i.getObject().reindexObject()
+
+def setupCountriesVocabulary(context):
+    vocabs['eea.soer.vocab.european_countries'] = []
+    util = queryUtility(ICountryAvailability)
+    all_countries = util.getCountries()
+    european_country_codes = countries.getCountries()
+    for i in european_country_codes:
+        country_name = all_countries[i][u'name']
+        vocabs['eea.soer.vocab.european_countries'].append((i, country_name))
+
+def setupTransform(context):
+    portal = context.getSite()
+    ptr = getToolByName(portal, 'portal_transforms')
+    if 'image_with_source' not in ptr.objectIds():
+        ptr.registerTransform(transform.register())
+
+def hideFromNavigation(context):
+    portal = context.getSite()
+    props = getToolByName(portal, 'portal_properties')
+    portalTypes = ['CommonalityReport', 'FlexibilityReport', 'DiversityReport', 'RelatedIndicatorLink']
+    hidden =  list(props.navtree_properties.metaTypesNotToList)
+    for t in portalTypes:
+        if t not in hidden:
+            hidden.append(t)
+    props.navtree_properties.manage_changeProperties(metaTypesNotToList=hidden)
+    
+def setupATVocabularies(context):
+    """ Installs all AT-based Vocabularies """
+
+    if context.readDataFile('eeasoer.txt') is None:
+        return
+    
+    # if we have eeasoer_vocabularies.txt vocabularies are replaced
+    # used when vocabularies need to be upgraded
+    replace = bool(context.readDataFile('eeasoer_vocabularies.txt'))
+    
+    from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
+    portal = context.getSite()
+    atvm = getToolByName(portal, ATVOCABULARYTOOL, None)
+    if atvm is None:
+        return
+    setupCountriesVocabulary(context)
+    for vkey in vocabs.keys():
+        if hasattr(atvm, vkey):
+            if not replace:
+                continue
+            atvm.manage_delObjects(ids=[vkey])
+        
+        print "adding vocabulary %s" % vkey
+        
+        atvm.invokeFactory('SimpleVocabulary', vkey)
+        simple = atvm.getVocabularyByName(vkey)
+        for (key, val) in vocabs[vkey]:
+            simple.addTerm(key, val)
+
+def setupVarious(context):
+    # only run this step if we are in eea.dataservice profile
+    if context.readDataFile('eeasoer.txt') is None:
+        return
+    setupTransform(context)
+    hideFromNavigation(context)
+
