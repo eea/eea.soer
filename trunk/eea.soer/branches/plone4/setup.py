f2072d731adb0664901c87afcfabc226b1d2b31b
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-31 12:28:36 +0000

    i18n domains updated setup.py refs 4300

--- trunk/eea.soer/branches/plone4/setup.py
+++ trunk/eea.soer/branches/plone4/setup.py
@@ -1,25 +1,27 @@
+""" EEA Soer installer
+"""
 from setuptools import setup, find_packages
 import os
-from os.path import join
 
-name = 'eea.soer'
-path = name.split('.') + ['version.txt']
-version = open(join(*path)).read().strip()
+NAME = 'eea.soer'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
-setup(name='eea.soer',
-      version=version,
+setup(name=NAME,
+      version=VERSION,
       description="",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='SENSE soer eea stateoftheenvironment rdf linkeddata',
       author='European Environment Agency',
       author_email='webadmin@eea.europa.eu',
-      url='http://svn.eionet.europa.eu/projects/Zope',
+      url="https://svn.eionet.europa.eu/projects/"
+          "Zope/browser/trunk/eea.soer",
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['eea'],
@@ -32,7 +34,7 @@ setup(name='eea.soer',
           'eea.vocab',
           'eea.rdfmarshaller',
           'eea.facetednavigation',
-          'eea.faceted.inheritance',          
+          'eea.faceted.inheritance',
           'Products.ATVocabularyManager',
           'lxml'
       ],
