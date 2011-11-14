8aa0ccb80cec0d4d682b0ba584306082bbbc6e51
tiberich <tiberiuichim@users.noreply.github.com>
2011-11-14 16:23:55 +0000

    Refs #8677; Move current trunk to plone25 branch

---
+++ eea/soer/browser/sense.py
@@ -0,0 +1,30 @@
+""" sense module
+"""
+from Products.Marshall.registry import getComponent
+
+
+class RDFExport(object):
+    """ RDFExport class"""
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        marshaller = getComponent('surfrdf')
+        _content_type, _length, data = marshaller.marshall(self.context)
+        self.request.response.setHeader(
+                'Content-Type','application/rdf+xml; charset=utf-8')
+        return data
+
+class ChannelInfo(object):
+    """ ChannelInfo class"""
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        """ return channel info """
+        pass
+        
