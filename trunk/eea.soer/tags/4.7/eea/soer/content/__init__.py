596855deb503ff051ee97fc7d19a30512fe67409
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-06-13 12:00:26 +0000

    removed wrong tag

--- trunk/eea.soer/tags/4.7/eea/soer/content/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Content
-"""
-#from eea.soer.content.SOERCountry import SOERCountry
-#from eea.soer.content.CommonalityReport import CommonalityReport
-#from eea.soer.content.DiversityReport import DiversityReport
-#from eea.soer.content.FlexibilityReport import FlexibilityReport
